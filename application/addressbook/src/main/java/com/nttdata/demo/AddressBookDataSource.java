9cb66f73b332e960af4bc2ef984f55628d11c553
qxb8497 <krzysztof.wolf@nttdata.com>
2019-11-06 18:21:13 +0100

    switch to payara-micro, enabled debug in local

--- application/addressbook/src/main/java/com/nttdata/demo/AddressBookDataSource.java
+++ application/addressbook/src/main/java/com/nttdata/demo/AddressBookDataSource.java
@@ -4,13 +4,12 @@ package com.nttdata.demo;
 import javax.annotation.sql.DataSourceDefinition;
 
 @DataSourceDefinition(
-    name="java:global/jdbc/demo",
-    className="org.postgresql.ds.PGSimpleDataSource",
-    properties=
-        { "url=jdbc:postgresql://${DB_HOST}:5432/${DB_NAME}",
-          "user=${DB_USER}",
-          "password=${DB_PASS}" }
-)
+    name = "java:global/jdbc/demo",
+    className = "org.postgresql.ds.PGSimpleDataSource",
+    databaseName = "${ENV=DB_NAME}",
+    serverName = "${ENV=DB_HOST}",
+    user = "${ENV=DB_USER}",
+    password = "${ENV=DB_PASS}")
 public class AddressBookDataSource {
 
 }
